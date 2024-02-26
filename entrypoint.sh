52469070a2b9232ecb86c761ccc73fa1177e0036
nlemberg <naama.lemberg@gmail.com>
2024-02-26 16:57:45 +0200

    modified entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-max_attempts=12
+max_attempts=6
 attempt=0
 
 while [ $attempt -lt $max_attempts ]; do
   if nc -z mysql 3306; then
-    flask db upgrade
-    flask run --host=0.0.0.0
+    echo "Connected to mysql:3306."
     break
   else
     sleep 5
@@ -15,5 +14,8 @@ while [ $attempt -lt $max_attempts ]; do
 done
 
 if [ $attempt -eq $max_attempts ]; then
-  echo "Connection to mysql:3306 could not established."
+  echo "Connection to mysql:3306 could not established. Working locally with sqlite."
 fi
+
+flask db upgrade
+flask run --host=0.0.0.0
